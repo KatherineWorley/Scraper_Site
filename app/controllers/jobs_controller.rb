c943fc9ba65205bcb2374167cf4fb15c0d4f36c8
Katherine Worley <kdesign216@yahoo.com>
2017-12-07 19:16:52 -0500

    added scraper

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,17 @@
 class JobsController < ApplicationController
+
 	before_action :find_job, only: [:show, :edit, :update, :destroy]
 
+
 	def index
-		if params[:category].blank?
-			@jobs = Job.all.order("created_at DESC")
-		else
-			@category_id = Category.find_by(name: params[:category]).id 
-			@jobs = Job.where(category_id: @category_id).order("created_at DESC") 
-		end 
+		@jobs = Job.scraper
+
+		# if params[:category].blank?
+		# 	@jobs = Job.all.order("created_at DESC")
+		# else
+		# 	@category_id = Category.find_by(name: params[:category]).id 
+		# 	@jobs = Job.where(category_id: @category_id).order("created_at DESC") 
+		# end 
 	end
 
 	def show
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,13 @@
 class Job < ApplicationRecord
+	include HTTParty
+	include Nokogiri
+
 	belongs_to :category
+
+	def self.scraper
+		url = "https://miami.craigslist.org/search/sof"
+		response = HTTParty.get url
+		dom = Nokogiri::HTML(response.body)
+		return dom.css("a.hdrlnk")
+	end
 end
